1ade9d982103561f8779153a6d75122772892e90
Don Kackman <dkackman@gmail.com>
2013-11-21 07:27:53 -0600

    decouple key from unit test project

--- SunlightAPITests/APIKEY.cs
+++ SunlightAPITests/APIKEY.cs
@@ -3,12 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace SunlightAPITests
 {
     static class APIKEY
     {
-#error enter your key here http://sunlightfoundation.com/api/accounts/register/
-        public const string Key = "";
+//#error either place you key in the file loaded below or return it directly here
+//#warning http://sunlightfoundation.com/api/accounts/register/
+        private static readonly string _key;
+
+        static APIKEY()
+        {
+            try
+            {
+                using (var file = File.OpenRead(@"D:\temp\sunlight.key.txt"))
+                using (var reader = new StreamReader(file))
+                    _key = reader.ReadLine();
+            }
+            catch
+            {
+                _key = "";
+            }
+        }
+
+        public static string Key
+        {
+            get
+            {
+                return _key;
+            }
+        }
     }
 }
